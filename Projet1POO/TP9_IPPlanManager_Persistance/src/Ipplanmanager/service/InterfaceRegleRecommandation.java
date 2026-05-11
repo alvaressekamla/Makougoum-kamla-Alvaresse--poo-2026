04e122414a344df86d48bc275792668c57400e1a
alvaressekamla <kamlaalvaresse@gmail.com>
2026-05-11 15:49:11 +0100

    Delete Projet1POO/TP9_IPPlanManager_Persistance directory

--- Projet1POO/TP9_IPPlanManager_Persistance/src/Ipplanmanager/service/InterfaceRegleRecommandation.java
+++
@@ -1,9 +0,0 @@
-package ipplanmanager.service;
-
-import ipplanmanager.model.Recommandation;
-import ipplanmanager.model.VLAN;
-
-public interface InterfaceRegleRecommandation {
-
-    Recommandation analyser(VLAN vlan);
-}
